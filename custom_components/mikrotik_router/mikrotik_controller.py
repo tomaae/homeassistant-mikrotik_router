136daa7c08da7f541f7c6ac2a74da6e2ee64e3ff
tomaae <23486452+tomaae@users.noreply.github.com>
2020-12-02 11:43:28 +0100

    Temp disabled uptime sensor

--- custom_components/mikrotik_router/mikrotik_controller.py
+++ custom_components/mikrotik_router/mikrotik_controller.py
@@ -718,15 +718,15 @@ class MikrotikControllerData:
             ],
         )
 
-        l = list(map(int, re.split('[whms]', self.data["resource"]["uptime_str"])[:-1]))
-        if len(l) == 4:
-            self.data["resource"]["uptime"] =  l[0] * 604800 + l[1] * 3600 + l[2] * 60 + l[3]
-        elif len(l) == 3:
-            self.data["resource"]["uptime"] =  l[0] * 3600 + l[1] * 60 + l[2]
-        elif len(l) == 2:
-            self.data["resource"]["uptime"] =  l[0] * 60 + l[1]
-        else:
-            self.data["resource"]["uptime"] = l[0]
+        # l = list(map(int, re.split('[wdhms]', self.data["resource"]["uptime_str"])[:-1]))
+        # if len(l) == 4:
+        #     self.data["resource"]["uptime"] =  l[0] * 604800 + l[1] * 3600 + l[2] * 60 + l[3]
+        # elif len(l) == 3:
+        #     self.data["resource"]["uptime"] =  l[0] * 3600 + l[1] * 60 + l[2]
+        # elif len(l) == 2:
+        #     self.data["resource"]["uptime"] =  l[0] * 60 + l[1]
+        # else:
+        #     self.data["resource"]["uptime"] = l[0]
 
         if self.data["resource"]["total-memory"] > 0:
             self.data["resource"]["memory-usage"] = round(
